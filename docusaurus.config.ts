6ec2e61416db2d5109dbc71fda8e83cf44a726a7
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2025-04-16 16:10:33 +0200

    fix: rename to ascii-ui-docs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: 'https://rcasia.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/ascii-ui/',
+  baseUrl: '/ascii-ui-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
