05e33ad95b19e46d5cc7d657bf70d93f1c149525
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2025-10-04 17:26:15 +0200

    feat: add lua and bash to additionalLanguages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -99,6 +99,7 @@ const config: Config = {
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
+      additionalLanguages: ['lua', 'bash'],
     },
   } satisfies Preset.ThemeConfig,
 };
