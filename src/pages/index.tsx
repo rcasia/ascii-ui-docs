636467410372b5b0c42e63ac590a73029a5a055f
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2025-10-04 20:07:32 +0200

    fix: broken link to get started

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ function HomepageHeader() {
       <div className="container">
         <Heading as="h1" className="hero__title ascii-title" >
           <span aria-label="ASCII UI homepage">
-          {`
+            {`
  ▗▄▖  ▗▄▄▖ ▗▄▄▖▗▄▄▄▖▗▄▄▄▖    ▗▖ ▗▖▗▄▄▄▖
 ▐▌ ▐▌▐▌   ▐▌     █    █      ▐▌ ▐▌  █  
 ▐▛▀▜▌ ▝▀▚▖▐▌     █    █      ▐▌ ▐▌  █  
@@ -28,7 +28,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/docs/get-started">
             Get Started
           </Link>
         </div>
