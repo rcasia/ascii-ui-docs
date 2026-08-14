42a9d1aac77fa6a525f81e4277233575a6985b7b
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2026-08-14 15:12:55 +0200

    fix: update baseUrl for repo rename to ascii-ui.github.io

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: 'https://ascii-ui.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: process.env.NODE_ENV === 'development' ? '/' : (process.env.DEV_BASE_URL || '/ascii-ui-docs/'),
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
